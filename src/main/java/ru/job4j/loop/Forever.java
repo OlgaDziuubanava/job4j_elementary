cbd203c88ae80c7bbcf49ec47a2d5e289b772e18
olgadziuubanava <dziuubanavaolga@gmail.com>
2026-03-15 13:42:29 +0100

    5.0.2. Вечный цикл for. [#226417]

---
+++ src/main/java/ru/job4j/loop/Forever.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Forever {
+    public static void consol() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Forever");
+        }
+    }
+
+    public static void main(String[] args) {
+        consol();
+    }
+}
